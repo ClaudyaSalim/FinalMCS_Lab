e2cc7ad82d9de49f50aa8b5bfba949524b4eb8a7
christineputri <89205581+christineputri@users.noreply.github.com>
2023-06-08 14:22:44 +0700

    otp xml

---
+++ app/src/main/java/com/example/bluejack_pharmacy_final_mcs/otp.java
@@ -0,0 +1,14 @@
+package com.example.bluejack_pharmacy_final_mcs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class otp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}
